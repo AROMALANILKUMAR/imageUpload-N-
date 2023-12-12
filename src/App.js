5db902977b9f61988bb5043c043e98cb32633707
AROMALANILKUMAR <aromalanilkumar.621821@gmail.com>
2023-12-12 08:56:05 +0530

    done

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Displayimage from "./components/Displayimage"
+
+function App(){
+  return (
+    <div>
+      <h1>Upload Images</h1>
+      <Displayimage/>
+    </div>
+  )
+}
+
+export default App;
